453cc9ef79a0fd413bf317a9acf85036bf222ddf
Harsh Patel <yoharsh113@gmail.com>
2022-05-10 13:29:24 +0530

    Filler

--- Routes/CoursesDetails.js
+++ Routes/CoursesDetails.js
@@ -3,5 +3,6 @@ const router = express.Router(); // create express router
 const CoursesDetails = require('../controllers/CoursesDetails_controller'); // import live courses controller
 
 router.get('/', CoursesDetails.getCoursesDetails); // get live courses')
+router.get('/CourseDetail', CoursesDetails.getCoursesDetails); // get live courses')
 
 module.exports = router; // export router
